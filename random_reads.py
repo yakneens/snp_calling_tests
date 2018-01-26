73bcc1c803f6126de7669380fbaa1751dc137514
Sergei Iakhnin <llevar@gmail.com>
2018-01-26 11:48:44 +0100

    Updates

--- random_reads.py
+++ random_reads.py
@@ -1,9 +1,38 @@
 import pysam
 from math import log10
 from random import shuffle
+import timeit
 
+class Locus:
+    def __init__(self, pos, ref, alt, gl_ref, gl_het, gl_hom, history):
+        self.pos = pos
+        self.ref = ref
+        self.alt = alt
+        self.gl_ref = gl_ref
+        self.gl_het = gl_het
+        self.gl_hom = gl_hom
+        self.history = history
+        #Read Depth
+        self.dp = 0
+        #Reference observation count
+        self.ro = 0
+        #Sum of reference observation qualities
+        self.qr = 0
+        #Alternate observation count
+        self.ao = 0
+        #Sum of alternate observation qualities
+        self.qa = 0
+        
+    def add_to_history(self, new_val):
+        self.history.append(new_val)
 
-
+    def get_gls(self):
+        return (self.gl_ref, self.gl_het, self.gl_hom)
+    
+    def get_norm_gls(self):
+        norm_const = self.gl_ref + self.gl_het + self.gl_hom
+        return (self.gl_ref / norm_const, self.gl_het / norm_const, self.gl_hom / norm_const)
+    
 def to_phred(x):
     return -10*(log10(x))
 
@@ -13,143 +42,121 @@ def from_phred(x):
 def get_priors():
     heterozygosity = 0.8 * 0.001
     prior_1 = heterozygosity
-    prior_0 = pow(heterozygosity, 2) #should this be theta over 2?
+    prior_0 = pow(heterozygosity, 2)
     prior_2 = 1 - prior_1 - prior_0
     return (prior_0, prior_1, prior_2)
 
 (prior_0, prior_1, prior_2) = get_priors()
 
-my_start = 10000000
-my_stop = 10001000
-my_chr = "20"
-infile = "/Users/siakhnin/data/CEUTrio.HiSeq.WGS.b37.NA12878.20.21.bam"
-ref =  pysam.FastaFile("/Users/siakhnin/data/human_g1k_v37.20.21.fasta").fetch(region="{}:{}-{}".format(my_chr, my_start + 1, my_stop + 1))
-samfile = pysam.AlignmentFile(infile, "rb")
-reads = samfile.fetch(region="{}:{}-{}".format(my_chr, my_start + 1, my_stop + 1))
-my_reads = [a for a in reads]
-shuffle(my_reads, lambda: 0.4)
-sites = {}
-my_snp = []
-#rec - (buf, gl_0, gl_1, gl_2, ref, alt)
 
-def handle_match(read, read_idx, match_len, ref_offset):
-#    print(read.query_alignment_sequence[read_idx:read_idx+match_len])
+def handle_match(read, read_idx, match_len, ref_offset, all_loci):
     
     seq = read.query_alignment_sequence[read_idx:read_idx+match_len]
     qual = read.query_alignment_qualities[read_idx:read_idx+match_len]
-    ref = read.get_reference_sequence()[ref_offset:ref_offset+match_len]
-    
-    flag = True
+    read_ref = read.get_reference_sequence()[ref_offset:ref_offset+match_len]
     
     for i in range(len(seq)):
         cur = read.pos + read_idx + i
         
-        my_rec = sites.get(cur)
-        if not my_rec:
-            my_rec = [[], [prior_0, prior_1, prior_2], ref[i], None, cur+1]
+        my_locus = all_loci.get(cur)
+        if not my_locus:
+            my_locus = Locus(pos=cur+1, ref=read_ref[i], alt=None, gl_ref=prior_2, gl_het=prior_1, gl_hom=prior_0, history=[])
         
         p_error = from_phred(qual[i])
         
-        my_rec[1][1] = my_rec[1][1] * 0.5
-        my_rec[0].append((seq[i], ref[i], p_error, read.qname))
-        if(seq[i] != ref[i]):
-            if flag:
-                print(read)
-                flag = False    
-            print (seq[i], qual[i], ref[i])
-            
-            my_rec[3] = seq[i]
-            my_rec[1][2] = my_rec[1][2] * p_error
-            my_rec[1][0] = my_rec[1][0] * (1 - p_error)
+        my_locus.dp += 1
+        
+        my_locus.gl_het *= 0.5
+        
+        if(seq[i] != read_ref[i]):
+            my_locus.alt = seq[i]
+            my_locus.gl_ref *= p_error
+            my_locus.gl_hom *= (1 - p_error)
+            my_locus.ao += 1
+            my_locus.qa += qual[i]
+
         else:
-            my_rec[1][2] = my_rec[1][2] * (1 - p_error)
-            my_rec[1][0] = my_rec[1][0] * p_error
-        
-        sites[cur] = my_rec
+            my_locus.gl_ref *= (1 - p_error)
+            my_locus.gl_hom *= p_error
+            my_locus.ro += 1
+            my_locus.qr += qual[i]
 
-others = []    
-a = ['20GAVAAXX100126:7:64:9392:84449']
-for read in my_reads:
-    read_name = read.qname
+        my_locus.add_to_history(my_locus.gl_ref)        
+        
+        all_loci[cur] = my_locus
 
-    if read_name in a:
-        print("Found")
-    if not read.is_duplicate and not read.is_qcfail and not read.mapping_quality == 0:
-        
-        cigar_els = read.cigartuples
-        
-        if not cigar_els:
-            print("Read {} has bad CIGAR {}, skipping".format(read.qname, read.cigar))
-            continue
+
+
+def process_reads(my_reads, all_loci):
+    for read in my_reads:
+        if not read.is_duplicate and not read.is_qcfail and not read.mapping_quality == 0:
             
-        ref_offset = 0
-        
-        read_idx = 0
-        for el in cigar_els:
-            el_type = el[0]
-            el_len = el[1]
+            cigar_els = read.cigartuples
             
-            if el_type == 0 or el_type == 2:
-                if el_type == 0:
-                    handle_match(read, read_idx, el_len, ref_offset)
-                ref_offset = ref_offset + el_len
-            if el_type != 2 and el_type != 4:
-                read_idx = read_idx + el_len
+            if not cigar_els:
+                print("Read {} has bad CIGAR {}, skipping".format(read.qname, read.cigar))
+                continue
+                
+            ref_offset = 0
+            
+            read_idx = 0
+            for el in cigar_els:
+                el_type = el[0]
+                el_len = el[1]
+                
+                if el_type == 0 or el_type == 2:
+                    if el_type == 0:
+                        handle_match(read, read_idx, el_len, ref_offset, all_loci)
+                    ref_offset += el_len
+                if el_type != 2 and el_type != 4:
+                    read_idx += el_len
 
-print_results = True
-for pos in sorted(sites):
-    rec = sites[pos]
-    gls = rec[1]
-    ref = rec[2]
-    alt = rec[3]
-    #pos = rec[4]
-    reads = rec[0]
-    norm = sum(gls)
-    gls = [gl/norm for gl in gls]
-    (post_0, post_1, post_2) = gls
+def call_variants(input_file, 
+                  reference_file, 
+                  output_file,
+                  my_start,
+                  my_stop,
+                  my_chr, 
+                  print_results=True):
     
-    vqual = 0
-    if post_2 != 0:
-        vqual = to_phred(post_2)
-    else:
-        vqual = 5000
+    ref =  pysam.FastaFile(reference_file).fetch(region="{}:{}-{}".format(my_chr, my_start + 1, my_stop + 1))
+    samfile = pysam.AlignmentFile(input_file, "rb")
+    reads = samfile.fetch(region="{}:{}-{}".format(my_chr, my_start + 1, my_stop + 1))
+    my_reads = [a for a in reads]
+    #shuffle(my_reads, lambda: 0.4)
+    shuffle(my_reads)
+    all_loci = {}
+    my_snp = []
+    
+    process_reads(my_reads, all_loci)
+    
+    for cur_pos in sorted(all_loci):
+        cur_locus = all_loci[cur_pos]
+        (post_ref, post_het, post_hom)  = cur_locus.get_norm_gls()
         
-    if vqual > 20:   
-        res = (pos+1, 0 if post_0 > post_1 else 1, ref, alt, vqual, post_2, post_1, post_0)
-        my_snp.append(rec)
-        #print pos
-        if print_results:
-            print res
+        vqual = 0
+        if post_ref != 0:
+            vqual = to_phred(post_ref)
+        else:
+            vqual = 5000
             
-        n_gl_0 = 1
-        n_gl_1 = 1
-        n_gl_2 = 1
-        read_ids = []   
-        for read in reads:
-            n_gl_1 = n_gl_1 * 0.5
-            if(read[0] == read[1]):
-                n_gl_0 = n_gl_0 * read[2]
-                n_gl_2 = n_gl_2 * (1 - read[2])            
-            else:
-                n_gl_2 = n_gl_2 * read[2]
-                n_gl_0 = n_gl_0 * (1 - read[2])
-            
-            read_ids.append(read[3])
-        
-        n_gl_0 = n_gl_0 * prior_0
-        n_gl_1 = n_gl_1 * prior_1
-        n_gl_2 = n_gl_2 * prior_2
-        
-        norm = n_gl_0 + n_gl_1 + n_gl_2
-             
-        n_gl_0 = n_gl_0 / norm
-        n_gl_1 = n_gl_1 / norm
-        n_gl_2 = n_gl_2 / norm
-        
-        #print "{} {} {}".format(n_gl_2, n_gl_1, n_gl_0)
-        print read_ids
-              
-#print "Other CIGARs {}.".format(others)
-#a = sites[10000438]
-#a_sum = a[1][0] + a[1][1] + a[1][2]    
-#print "{} {} {} {}".format(a, a[1][0] / a_sum, a[1][1] / a_sum, a[1][2] / a_sum)
+        if vqual > 20:   
+            res = (cur_pos + 1, 0 if post_hom > post_het else 1, cur_locus.ref, cur_locus.alt, vqual, post_ref, post_het, post_hom)
+            my_snp.append(cur_locus)
+            probs = cur_locus.history
+            if print_results:
+                print res
+                #print probs
+
+def call_test():
+    call_variants(my_start = 10000000,
+                  my_stop = 10001000,
+                  my_chr = "20",
+                  input_file = "/Users/siakhnin/data/CEUTrio.HiSeq.WGS.b37.NA12878.20.21.bam",
+                  reference_file = "/Users/siakhnin/data/human_g1k_v37.20.21.fasta",
+                  output_file = None)
+    
+print timeit.timeit(call_test,number=1)
+
+in_vcf = pysam.VariantFile("/Users/siakhnin/data/CEUTrio.HiSeq.WGS.b37.NA12878.20.21.freebayes.vcf", "r")
+out_vcf = pysam.VariantFile("/Users/siakhnin/data/CEUTrio.HiSeq.WGS.b37.NA12878.20.21.rheos.vcf", "w", header=in_vcf.header)
